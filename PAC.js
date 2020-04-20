fad944f505f65db8ccf83a3e7e1ff1453bc4af53
tuankietbl <63893381+tuankietbl@users.noreply.github.com>
2020-04-20 20:17:43 +0700

    Update PAC.js

--- PAC.js
+++ PAC.js
@@ -1,18 +1,14 @@
-var DIRECT = "DIRECT";
-var PROXY = "PROXY 127.0.0.1:80";
-var blacklist = {"appldnld.apple.com":1,"gdmf.apple.com":1,"ocsp.apple.com":1};
 function FindProxyForURL(url, host) {
+  var DIRECT = "DIRECT";
+  var PROXY = "PROXY 127.0.0.1:80";
+  var blacklist = {"appldnld.apple.com":1,"gdmf.apple.com":1,"ocsp.apple.com":1};
+  var index = host.indexOf(".");
   host = host.toLowerCase();
-  for (i = 0; i < 30; i++) {
-    if (blacklist[host]) {
-      return PROXY;
-    }
-    var index = host.indexOf(".");
-    if (index == -1) {
-      break;
-    } else {
-      host = host.substring(index + 1);
-    }
+  for (i = 0; i < 10; i++) {
+    if (blacklist[host])
+      {return PROXY;}
+    if (index == -1)
+      {break;} else {host = host.substring(index + 1);}
   }
   return DIRECT;
 }
