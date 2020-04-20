ebd7c4058ab1caaadbb1ad54edbea79dc38a56ac
tuankietbl <63893381+tuankietbl@users.noreply.github.com>
2020-04-20 21:44:44 +0700

    Update PAC.js

--- PAC.js
+++ PAC.js
@@ -1,12 +1,12 @@
 function FindProxyForURL(url, host) {
-  var blacklist = {"appldnld.apple.com":1,"gdmf.apple.com":1,"ocsp.apple.com":1};
+  var blacklist = {"ocsp.apple.com":1,"appldnld.apple.com":1,"gdmf.apple.com":1};
   var index = host.indexOf(".");
+  url = url.toLowerCase();
   host = host.toLowerCase();
-  for (i = 0; i < 5; i++) {
-    if (blacklist[host])
-      {return "PROXY 127.0.0.1:80";}
-    if (index == -1)
-      {break;} else {host = host.substring(index + 1);}
+  for (i = 0; i < 6; i++) {
+    if (shExpMatch(url, "https://mesu.apple.com/assets/com_apple_MobileAsset_SoftwareUpdate/com_apple_MobileAsset_SoftwareUpdate.xml")) {return "PROXY 127.0.0.1:80";}
+    if (blacklist[host]) {return "PROXY 127.0.0.1:80";}  
+    if (index == -1) {break;} else {host = host.substring(index + 1);}
   }
   return "DIRECT";
 }
